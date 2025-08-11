18cc8bf955bd5039ae9d5a5b84bbee9e71d5b7eb
Hubert Shelley <46239302+hubertshelley@users.noreply.github.com>
2025-08-11 09:14:14 +0800

    fix(examples): 修复grpc相关示例
    我修复grpc、grpc_h2c、grpc_streaming三个示例\n- 同步更新各示例的Cargo.toml与build.rs\n- 解决示例构建与运行问题，确保cargo check通过

--- examples/grpc/build.rs
+++ examples/grpc/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .compile_protos(&["proto/hello_world.proto"], &["/proto"])
         .unwrap();
 }
--- examples/grpc_h2c/build.rs
+++ examples/grpc_h2c/build.rs
@@ -3,7 +3,7 @@ use std::{env, path::PathBuf};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("hello_world_descriptor.bin"))
         .compile_protos(&["proto/hello_world.proto"], &["/proto"])
         .unwrap();
